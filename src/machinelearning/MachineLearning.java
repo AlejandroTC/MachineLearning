e1aaa90dc4cf659552617c78aabe466b7e9fe8a5
AlejandroTC <31549601+AlejandroTC@users.noreply.github.com>
2023-02-16 20:26:27 -0800

    PCescritorio
    Inicio creacion de Tokenizador

---
+++ src/machinelearning/MachineLearning.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package machinelearning;
+
+import clasificadores.MinimaDistancia;
+import data.Herramientas;
+import data.Patron;
+import data.Entrenamiento;
+import data.PatronRepresentativo;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author roban
+ */
+public class MachineLearning {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*Patron a = new Patron(new double[]{4,1}, "A");
+        Patron a1 = new Patron(new double[]{6,9}, "A");
+        Patron a2 = new Patron(new double[]{5.6,9}, "B");
+        Patron a3 = new Patron(new double[]{6,9.45}, "B");
+        Patron a4 = new Patron(new double[]{23,0.11}, "A");
+        Patron a5 = new Patron(new double[]{23,0.11}, "C");
+        Patron a6 = new Patron(new double[]{23,0.11}, "F");
+        Patron a7 = new Patron(new double[]{23,0.11}, "F");
+        //Patron b = new Patron(new double[]{7,8}, "B");
+        ArrayList<Patron> lista = new ArrayList<>();
+        lista.add(a);
+        lista.add(a1);
+        lista.add(a2);
+        lista.add(a3);
+        lista.add(a4);
+        lista.add(a5);
+        lista.add(a6);
+        lista.add(a7);*/
+        Entrenamiento.lectura();
+        /*Patron des = new Patron(new double[]{2.34,1.45},"Desconocido");
+        MinimaDistancia md = new MinimaDistancia();
+        md.entrenar(lista);
+        md.clasificar(des);*/
+        System.out.println();
+    }
+    
+}
