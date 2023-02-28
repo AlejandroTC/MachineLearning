85074d52248ed59170c77798e07974b743e2674a
AlejandroTC <aletamayo-02@hotmail.com>
2023-02-28 07:49:36 -0600

    Knn

---
+++ src/clasificadores/Knn.java
@@ -0,0 +1,9 @@
+package clasificadores;
+/*
+ * 1.- Distancia para inferencias importantes
+ * 2.- La construcción de vecinidades
+ * 3.- Establecer una cantidad fija de vecinidad representativa para poder realizar la clasificación
+*/
+public class Knn {
+    
+}
